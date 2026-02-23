a4f044abd79eabae9e9eed6c144e02ebd4d4f980
Paolo Gehant-Pereira <paolopereiragehant@gmail.com>
2026-02-23 23:25:02 +0100

    Minimal format of pages with bootstrap

--- app/BootstrapController.php
+++ app/BootstrapController.php
@@ -2,7 +2,34 @@
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Controller.php';
 
 class BootstrapController extends Controller {
+	/** @var array<string> $stylesheets */
+	protected array $stylesheets = array('bootstrap/css/bootstrap.min.css');
+
+	/** @var array<string> $scripts */
+	protected array $scripts = array('bootstrap/js/bootstrap.min.js');
+
 	public function __construct() {
-		
+		parent::__construct();
+
+		$stylesheets_html = '';
+		foreach ($this->stylesheets as $stylesheet) {
+			$stylesheets_html .= '<link href="/' . $stylesheet .'" rel="stylesheet"/>';
+		}
+
+		echo '<!doctype html>' .
+				'<html lang="en">' .
+				'<head>' .
+					'<meta charset="utf-8"/>' .
+					'<meta name="viewport" content="width=device-width, initial-scale=1"/>' .
+					$stylesheets_html .
+				'</head>' . 
+			'<body>';
+	}
+
+	function __destruct() {
+		foreach($this->scripts as $script) {
+			echo '<script src="/' . $script .'"></script>';
+		}
+		echo '</body></html>';
 	}
 }
